984efa9714e6e316cbc9a6af7056f81a8a1be019
Alex Fout <fout.alex@gmail.com>
2017-05-24 00:09:13 -0600

    debugged pca mode of embedding

--- embedding.py
+++ embedding.py
@@ -2,14 +2,15 @@ from sklearn.decomposition import PCA
 
 
 class Embedding(object):
-    def __init__(self, type="pca", ):
+    def __init__(self, type="pca", **kwargs):
         self.type = type
+        self.n_components = kwargs["n_components"]
 
     def train(self, train_data):
         if self.type == "pca":
-            pca = PCA()
+            pca = PCA(n_components=self.n_components)
             pca.fit(train_data)
-            self.pca= pca
+            self.pca = pca
 
     def embed(self, train_data):
         if self.type == "pca":
