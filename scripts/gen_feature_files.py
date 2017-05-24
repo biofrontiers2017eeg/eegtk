a236aef5dba17bdb4ba0c2acae26e3db543fff06
Alex Fout <fout.alex@gmail.com>
2017-05-24 02:22:10 -0600

    added epoch_size argument from config file

--- scripts/gen_feature_files.py
+++ scripts/gen_feature_files.py
@@ -1,4 +1,4 @@
-from config import pid_noConcussion, pid_3stepProtocol, pid_testRetest, pid_concussion, feature_functions
+from config import pid_noConcussion, pid_3stepProtocol, pid_testRetest, pid_concussion, feature_functions, epoch_size
 from patient import Patient
 
 # go through each list of ids
@@ -10,10 +10,10 @@ for lst in [pid_noConcussion, pid_3stepProtocol, pid_testRetest, pid_concussion]
         # generate file for pre_test
         if p.pre_test is not None:
             p.pre_test.remove_artifacts()
-            p.pre_test.get_examples(feature_functions, epoch_size=512)  # 2 seconds
+            p.pre_test.get_examples(feature_functions, epoch_size=epoch_size)
             p.pre_test.save_examples()
         if p.post_test is not None:
             # generate file for post_test
             p.post_test.remove_artifacts()
-            p.post_test.get_examples(feature_functions, epoch_size=512)  # 2 seconds
+            p.post_test.get_examples(feature_functions, epoch_size=epoch_size)
             p.post_test.save_examples()
