9935e1d472d41ac05b08b715961d278bdcb67b9c
Alex Fout <fout.alex@gmail.com>
2017-05-24 00:10:49 -0600

    added script to generate feature files for pre_test and post_test for each patient

---
+++ scripts/gen_feature_files.py
@@ -0,0 +1,19 @@
+from config import pid_noConcussion, pid_3stepProtocol, pid_testRetest, pid_concussion, feature_functions
+from patient import Patient
+
+# go through each list of ids
+for lst in [pid_noConcussion, pid_3stepProtocol, pid_testRetest, pid_concussion]:
+    # for each id...
+    for pid in lst:
+        print("Processing pid: {}".format(pid))
+        p = Patient(pid)
+        # generate file for pre_test
+        if p.pre_test is not None:
+            p.pre_test.remove_artifacts()
+            p.pre_test.get_examples(feature_functions, epoch_size=512)  # 2 seconds
+            p.pre_test.save_examples()
+        if p.post_test is not None:
+            # generate file for post_test
+            p.post_test.remove_artifacts()
+            p.post_test.get_examples(feature_functions, epoch_size=512)  # 2 seconds
+            p.post_test.save_examples()
